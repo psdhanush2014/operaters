db2518c9334a784e897632fecbf081ef6921f645
dhanush <psdhanush2014@gmail.com>
2025-09-07 12:14:28 -0500

    add anything

---
+++ demo.py
@@ -0,0 +1,14 @@
+Pettype = input("enter wat kind of pet u have dog or cat : ")
+
+match Pettype:
+
+ case 'dog':
+       print("woof")
+ case 'cat':
+       print("meow")
+
+ case _:
+      print("cool pet") 
+      
+      print("dhanush")
+
