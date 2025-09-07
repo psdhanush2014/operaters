09cea88ada2362d28d59454fd59ab7ca22655b0c
dhanush <psdhanush2014@gmail.com>
2025-09-07 12:21:35 -0500

    another match chase if you need

---
+++ fruitcase.py
@@ -0,0 +1,17 @@
+favoritefruit = input("enter your favorite fruit : ")
+
+match favoritefruit:
+
+ case "mango":
+     print("mango I eat a mango everyday at night in 8:25 ")
+ case "kiwi":
+     print("kiwi kiwi ninja is one of the most popular game developers")
+ case "dragon fruit":
+     print("dragon fruit dragon fruits are not actually fruits made from dragons")
+ case "apple":
+     print("apple and apple a day keeps the doctor away")
+ case "sugar apple":
+     print("sugar apple is a really good fruit")
+ 
+ case _:
+     print("invalid ")
