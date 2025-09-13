293c1cc679874a4fcb7a4529fbfb563f1633abfe
dhanush <psdhanush2014@gmail.com>
2025-09-13 11:59:02 -0500

    Add files via upload

---
+++ practiceprogramsifandelse.py
@@ -0,0 +1,64 @@
+ifschoolistoday = False
+
+if ifschoolistoday  :
+    print("he will go to school")
+else:
+    print("he will not go to school and relax")
+
+icecream = False
+
+if icecream  :
+    print("he will eat the ice cream")
+else:
+    print("he will go ask for some")
+
+ifsunny = True
+
+if ifsunny  :
+    print("he will go out side")
+else:
+    print("he will stay indoor")
+          
+ifhaveapet = True 
+
+if ifhaveapet  :
+    print("play with the pet")
+else:
+    print("he will draw his favorite animal")
+
+birthday = True
+
+if birthday  :
+    print("go eat a cake")
+else:
+    print("go wish someone else a happy birthday")
+
+rainingtacos = False
+
+if rainingtacos  :
+    print("take an umbrella with you")
+else:
+    print("go outside without an umbrella")
+
+
+
+day_date = int(input("enter a number thruogh (1-7):"))
+
+match day_date:
+case 1:
+print("sunday")
+case 2:
+print("monday")
+case 3:
+print("tuesday")
+case 4:
+print("wenesday")
+case 5:
+print("thursday")
+case 6:
+print("friday")
+case 7:
+print("saturday")
+
+case_:
+print("invalid number/day")
